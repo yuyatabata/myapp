eeb646ff0c38b20f80a4a2a8f6f5b299a2859925
田端佑也 <tabayu5@gmail.com>
2020-12-28 23:18:57 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useReducer } from 'react';
+
+import API, { graphqlOperation } from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+
+import { createTodo } from '/graphql/mutations';
+import { listTodos } from './graphql/queries';
+import { onCreateTod } from './graphql/subscriptions';
+
+import awsconfig from './aws-exports';
 import './App.css';
 
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+const QUERY = "QUERY";
+const SUBSCRIPTION = "SUBSCRIPTION";
+
+const initialState = {
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case QUERY:
+      return {...state, todos: action.todos};
+    case SUBSCRIPTION:
+      return {...state, todos:[...state.todos, action.todo]}
+    default:
+      return state;
+  }
+};
+
+async function createNewTodo() {
+  const todo = { name: "Use AWS AppSync", description: "Realtime and Offline" };
+  await API.graphql(graphqlOperation(createTodo, { input: todo }));
+}
+
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getData() {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      dispatch({ type: QUERY, todos: todoData.datalistTodos.items });
+    }
+    getData();
+
+    const subscription = API.graphql(graphqlOperation(onCreateTod)).subscribe({
+      next: (eventData) => {
+        const todo = eventData.value.data.onCreateTod;
+        dispatch({ type: SUBSCRIPTION, todo });
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createNewTodo}>Add Todo</button>
+      <div>
+        {state.todos.length > 0 ?
+          state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>):
+          <p>Add some todos!</p>
+        }
+      </div>
     </div>
   );
 }
